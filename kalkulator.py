d4c8d0c72f6f354a302bb7be0adc26aec8714a5b
Bartosz Rosol <bartoszrosol89@icloud.com>
2024-02-05 22:30:31 +0000

    Dodano kalkulator Python

---
+++ kalkulator.py
@@ -0,0 +1,19 @@
+def dodawanie(a, b):
+    return a + b
+
+def odejmowanie(a, b):
+    return a - b
+
+def mnozenie(a, b):
+    return a * b
+
+def dzielenie(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Nie można dzielić przez zero!"
+
+# Przykładowe użycie:
+x = 10
+y = 5
+print(dodawanie(x, y))
